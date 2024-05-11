c4e5bbdedfbdcebd62911926f2685485a6aaae8c
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2024-05-11 18:49:35 +0700

    Use cdfquantreg With (In) R Software

--- cdfquantreg/cdfquantreg.R
+++ cdfquantreg/cdfquantreg.R
@@ -4,25 +4,14 @@
 # Generalized linear models finite cdf quantile Use cdfquantreg With (In) R Software
 # General linear models using finite-tailed cdf-quantile distributions for variables on the closed unit interval Use cdfquantreg With (In) R Software
 # General linear models using cdf-quantile distributions for variables on the unit interval Use cdfquantreg With (In) R Software
-install.packages("readxl")
-install.packages("httr")
 install.packages("cdfquantreg")
 install.packages("lmtest")
 install.packages("copula")
-library("httr")
-library("readxl")
 library("cdfquantreg")
 library("lmtest")
 library("copula")
 # Import Data Excel Into R From Github Olah Data Semarang (timbulwidodostp)
-github_link <- "https://github.com/timbulwidodostp/cdfquantreg/raw/main/cdfquantreg/cdfquantreg.xlsx"
-temp_file <- tempfile(fileext = ".xlsx")
-req <- GET(github_link, 
-# authenticate using GITHUB_PAT
-authenticate(Sys.getenv("GITHUB_PAT"), ""),
-# write result to disk
-write_disk(path = temp_file))
-cdfquantreg <- readxl::read_excel(temp_file)
+cdfquantreg = read.csv("https://github.com/timbulwidodostp/cdfquantreg/raw/main/cdfquantreg/cdfquantreg.csv",sep = ";")
 # Estimate Generalized linear models finite cdf quantile Use cdfquantreg With (In) R Software
 cdfquantreg <- cdfquantreg(Q4 ~ 1 | 1, fd = "t2", sd = "t2", data = cdfquantreg)
 cdfquantreg
